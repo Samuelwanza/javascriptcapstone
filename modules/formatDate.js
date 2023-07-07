f293f1764576bcceca309a0c128a89bcffb46a75
Samuel wanza <s.wanza@alustudent.com>
2023-07-07 17:30:01 +0200

    solved eslint issue

--- modules/formatDate.js
+++ modules/formatDate.js
@@ -1,7 +1,7 @@
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear().toString();
   return `${day}/${month}/${year}`;
 };
