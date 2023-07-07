db19fa408322c684bd4cda1a608cc13d5858c5ab
Samuel wanza <s.wanza@alustudent.com>
2023-07-07 17:26:18 +0200

    tested the comments count

--- showCount.test.js
+++ showCount.test.js
@@ -1,7 +1,13 @@
-import showLikes from './modules/showLikes';
+import getComments from "./modules/getComments";
+import showLikes from "./modules/showLikes";
 
-it('should count the number of shows', async () => {
+it("should count the number of shows", async () => {
   const likes = await showLikes();
   const count = likes.length;
   expect(count).toEqual(15);
+});
+it("should count the number of comments for the show, Person of interest", async () => {
+  const comments = await getComments(2);
+  const count = comments.length;
+  expect(count).toEqual(9);
 });
