2abb45b160b0733276e72a87e84765cddffefcf7
ctokoli <matumasie@gamil.com>
2023-07-07 13:29:07 +0000

    remove the width on all input

--- modules/showLikes.js
+++ modules/showLikes.js
@@ -2,6 +2,7 @@ import fetchAPI from './fetchAPI';
 
 const showLikes = async () => {
   const likes = await fetchAPI('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/PVsyyzJcjAI3kwRWNz7v/likes');
+  console.log(likes);
   return likes;
 };
 
