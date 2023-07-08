6d41651190ebbad61a30cb262b10e595063bf313
ctokoli <matumasie@gamil.com>
2023-07-08 08:10:35 +0000

    remove gitignore files

--- config.js
+++
@@ -1,3 +0,0 @@
-export const MOVIES_API = 'https://api.tvmaze.com/shows';
-export const LIKES_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/PVsyyzJcjAI3kwRWNz7v/likes';
-export const COMMENTS_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/PVsyyzJcjAI3kwRWNz7v';
