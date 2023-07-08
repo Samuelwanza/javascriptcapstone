a3f2ba4dd4e2e299b2efdbcad04cd21b9ce23c9e
ctokoli <matumasie@gamil.com>
2023-07-08 08:20:04 +0000

    fix liters error

---
+++ config.js
@@ -0,0 +1,3 @@
+export const MOVIES_API = 'https://api.tvmaze.com/shows';
+export const LIKES_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/PVsyyzJcjAI3kwRWNz7v/likes';
+export const COMMENTS_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/PVsyyzJcjAI3kwRWNz7v';
