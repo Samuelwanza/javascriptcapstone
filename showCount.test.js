d4fedc6d2afab2f12b5b28200c40fcd142e83dd4
ctokoli <matumasie@gamil.com>
2023-07-08 08:07:31 +0000

    test for edge cases

--- showCount.test.js
+++ showCount.test.js
@@ -1,13 +1,25 @@
 import getComments from './modules/getComments';
 import showLikes from './modules/showLikes';
 
-it('should count the number of shows', async () => {
-  const likes = await showLikes();
-  const count = likes.length;
-  expect(count).toEqual(15);
-});
-it('should count the number of comments for the show, Person of interest', async () => {
-  const comments = await getComments(2);
-  const count = comments.length;
-  expect(count).toEqual(9);
+describe('test counters', () => {
+  it('should count the number of shows', async () => {
+    const likes = await showLikes();
+    const count = likes.length;
+    expect(count).toEqual(15);
+  });
+  it('likes should return undefined ', async () => {
+    let likes = await showLikes();
+    likes = undefined;
+    expect(likes).toEqual(undefined);
+  });
+  it('should count the number of comments for the show, Person of interest', async () => {
+    const comments = await getComments(2);
+    const count = comments.length;
+    expect(count).toEqual(9);
+  });
+  it('comments should return undefined', async () => {
+    let comments = await getComments(2);
+    comments = undefined;
+    expect(comments).toEqual(undefined);
+  });
 });
