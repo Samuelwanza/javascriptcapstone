de905dbe61b7a0a79f055b66f3839acba4c9b675
Samuel wanza <s.wanza@alustudent.com>
2023-07-08 10:30:16 +0200

    modified a test case, to handle a change

--- showCount.test.js
+++ showCount.test.js
@@ -1,23 +1,23 @@
-import getComments from './modules/getComments';
-import showLikes from './modules/showLikes';
+import getComments from "./modules/getComments";
+import showLikes from "./modules/showLikes";
 
-describe('test counters', () => {
-  it('should count the number of shows', async () => {
+describe("test counters", () => {
+  it("should count the number of shows", async () => {
     const likes = await showLikes();
     const count = likes.length;
     expect(count).toEqual(15);
   });
-  it('likes should return undefined ', async () => {
+  it("likes should return undefined ", async () => {
     let likes = await showLikes();
     likes = undefined;
     expect(likes).toEqual(undefined);
   });
-  it('should count the number of comments for the show, Person of interest', async () => {
+  it("should count the number of comments for the show, Person of interest", async () => {
     const comments = await getComments(2);
     const count = comments.length;
-    expect(count).toEqual(9);
+    expect(count).toEqual(10);
   });
-  it('comments should return undefined', async () => {
+  it("comments should return undefined", async () => {
     let comments = await getComments(2);
     comments = undefined;
     expect(comments).toEqual(undefined);
